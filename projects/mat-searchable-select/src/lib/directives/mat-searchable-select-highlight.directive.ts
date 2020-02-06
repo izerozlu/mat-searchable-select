b5da6b9aa5fe9148de6b1ffddf325b904855db79
izerozlu <izerozlu@gmail.com>
2020-02-06 23:11:37 +0300

    MatSearchableSelectHighlightDirective's processing improved.

--- projects/mat-searchable-select/src/lib/directives/mat-searchable-select-highlight.directive.ts
+++ projects/mat-searchable-select/src/lib/directives/mat-searchable-select-highlight.directive.ts
@@ -1,11 +1,11 @@
-import {AfterViewInit, Directive, ElementRef, Input} from '@angular/core';
-import {BehaviorSubject} from "rxjs";
+import {AfterViewInit, Directive, ElementRef, Input, OnDestroy} from '@angular/core';
+import {BehaviorSubject, Subscription} from "rxjs";
 import {filter} from "rxjs/operators";
 
 @Directive({
 	selector: '[mat-searchable-select-highlight]'
 })
-export class MatSearchableSelectHighlightDirective implements AfterViewInit {
+export class MatSearchableSelectHighlightDirective implements AfterViewInit, OnDestroy {
 	@Input('mat-searchable-select-highlight') private isEnabled: boolean;
 
 	@Input('highlight-query')
@@ -16,6 +16,8 @@ export class MatSearchableSelectHighlightDirective implements AfterViewInit {
 	private defaultText: string;
 	private element: HTMLElement;
 	private searchQuery = new BehaviorSubject<string>('');
+	private subscriptions: Subscription[] = [];
+	private listItemContent: HTMLSpanElement;
 
 	constructor(
 		private elementRef: ElementRef
@@ -24,35 +26,41 @@ export class MatSearchableSelectHighlightDirective implements AfterViewInit {
 	}
 
 	public ngAfterViewInit() {
+		this.listItemContent = this.element.querySelector('.mat-searchable-select__list-item-content');
 		this.listenSearchQuery();
 	}
 
+	public ngOnDestroy() {
+		this.subscriptions.forEach((subscription: Subscription) => subscription.unsubscribe());
+	}
+
 	private listenSearchQuery() {
-		this.searchQuery.pipe(
-			filter(((searchQuery: string) => this.isEnabled && searchQuery && searchQuery.length > 0))
+		const subscription = this.searchQuery.pipe(
+			filter(((searchQuery: string) => this.isEnabled && searchQuery !== undefined))
 		).subscribe((searchQuery: string) => {
-			const matListItemElement = this.element.childNodes[0];
-			const childNodes = matListItemElement.childNodes;
-			const textNode = Array.from(childNodes).filter((node: Node) => node.nodeType === Node.TEXT_NODE)[0];
-			const textContent = textNode.textContent;
-			if (!this.defaultText) {
-				this.defaultText = textContent;
-			} else if (this.element.querySelector('.mat-searchable-select-highlight')) {
-				textNode.nextSibling.remove();
-				textNode.nextSibling.remove();
+			this.checkForDefaultText();
+			let listItemInnerHtml: string;
+			if (searchQuery && this.defaultText.includes(searchQuery)) {
+				const highlightElement: HTMLSpanElement = this.generateHighlightElement();
+				listItemInnerHtml = this.defaultText.replace(new RegExp(searchQuery, 'g'), highlightElement.outerHTML);
+			} else {
+				listItemInnerHtml = this.defaultText;
 			}
-			if ((this.defaultText || textContent).includes(searchQuery)) {
-				const highlightElement = this.generateHighlightElement();
-				const splitNodes = (this.defaultText ? document.createTextNode(this.defaultText) : textNode).nodeValue.split(searchQuery);
-				textNode.replaceWith(splitNodes[0], highlightElement, splitNodes[1]);
-			}
-		})
+			this.listItemContent.innerHTML = listItemInnerHtml;
+		});
+		this.subscriptions.push(subscription);
+	}
+
+	private checkForDefaultText() {
+		if (!this.defaultText) {
+			this.defaultText = this.listItemContent.textContent.trim();
+		}
 	}
 
 	private generateHighlightElement() {
 		const highlightElement: HTMLSpanElement = document.createElement('span');
 		highlightElement.classList.add('mat-searchable-select-highlight');
-		highlightElement.style.background = 'rgba(0, 0, 0, .06)';
+		highlightElement.style.background = 'rgba(0, 0, 0, .12)';
 		highlightElement.style.borderRadius = '4px';
 		highlightElement.textContent = this.searchQuery.value;
 		return highlightElement;
